2bc39347b3bff187663f56967dd0781c87dd82a5
cath <claire32186@gmail.com>
2020-07-01 23:08:42 +0800

    Update fullpage.js

--- 0701/dist/fullpage.js
+++ 0701/dist/fullpage.js
@@ -124,7 +124,7 @@
             scrollingSpeed: 700,
             autoScrolling: true,
             fitToSection: true,
-            fitToSectionDelay: 1000,
+            fitToSectionDelay: 0,
             easing: 'easeInOutCubic',
             easingcss3: 'ease',
             loopBottom: false,
