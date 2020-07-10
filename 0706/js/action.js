dc7d8642a5c5ed47bc817c64167b234041853f43
cath <claire32186@gmail.com>
2020-07-10 10:42:24 +0800

    0706

--- 0706/js/action.js
+++ 0706/js/action.js
@@ -51,5 +51,38 @@ $(function(){
 
 
 
+var $homeSlider = $(".home-slider");
+
+$(window).resize(function() {
+  showHomeSlider();
+});
+
+function showHomeSlider() {
+  if ($homeSlider.data("owlCarousel") !== "undefined") {
+    if (window.matchMedia('(min-width: 600px)').matches) {
+      initialHomeSlider();
+    } else {
+      destroyHomeSlider();
+    }
+  }
+}
+showHomeSlider();
+
+function initialHomeSlider() {
+  $homeSlider.addClass("owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    smartSpeed: 1000
+  });
+}
+
+function destroyHomeSlider() {
+  $homeSlider.trigger("destroy.owl.carousel").removeClass("owl-carousel");
+}
+
+
+
 
 
