b1986da2aba937b5e7f46af8e276c78f4793612a
cath <claire32186@gmail.com>
2020-10-06 16:26:47 +0800

    0930

--- 0930/js/action.js
+++ 0930/js/action.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $(".toggle-nav").click(function(e) {
         e.preventDefault();
         $(this).toggleClass("");
-        $(".menu-m ul").toggle();
+        $(".navigation ul").toggle();
     });
     adjustMenu();
 })
@@ -24,28 +24,31 @@ $(window).bind('resize orientationchange', function() {
 });
 var adjustMenu = function() {
     if (ww < 1024) {
-    $(".menu-m ul li").unbind('mouseenter mouseleave');
-    $(".menu-m ul li a").click(function(){
-            $(".menu-m ul").hide();
+    $(".navigation ul li").unbind('mouseenter mouseleave');
+    $(".navigation ul li a").click(function(){
+            $(".navigation ul").hide();
     }); 
     }
 }    
 
 //選單下滑動態
 $(function(){
-    $('#bt-qa, #bts-qa').click(function(){
+    $('#bt-kv, #bts-kv').click(function(){
+        $('body,html').animate({scrollTop:$('#kvc').offset().top}, 800);
+    });
+    $('#bt-qa, #bt-scroll').click(function(){
         $('body,html').animate({scrollTop:$('#qac').offset().top-50}, 800);
     });
-    $('#bt-cough, #bts-cough').click(function(){
+    $('#bt-cough').click(function(){
         $('body,html').animate({scrollTop:$('#coughc').offset().top+450}, 800);
     });
-    $('#bt-cancer, #bts-cancer').click(function(){
+    $('#bt-cancer').click(function(){
         $('body,html').animate({scrollTop:$('#cancerc').offset().top+50}, 800);
     });
-    $('#bt-food, #bts-food').click(function(){
+    $('#bt-food').click(function(){
         $('body,html').animate({scrollTop:$('#foodc').offset().top}, 800);
     });
-    $('#bt-dad, #bts-dad').click(function(){
+    $('#bt-dad').click(function(){
         $('body,html').animate({scrollTop:$('#dadc').offset().top}, 800);
     });
 });
