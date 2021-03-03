9a2e745c8110db09782b72e4e355d90623e276aa
cath <claire32186@gmail.com>
2021-03-03 12:58:35 +0800

    0227

--- 0227/js/action.js
+++ 0227/js/action.js
@@ -1,38 +1,3 @@
-//scrollfix
-var ww = $(window).width();
-$(document).ready(function() {
-    if (ww < 1024) {
-        var secondaryNav = $('.logopic'),
-        navHeight = secondaryNav.offset().top+200;
-    }
-    else {
-        var secondaryNav = $('.logopic'),
-        navHeight = secondaryNav.offset().top+1200;
-    }
-
-    $(".float").css({ "display":"none" });
-    $(window).on('scroll', function() {
-    
-        if ($(window).scrollTop() > navHeight) {
-            $('.scroll').addClass("fixed");
-        }
-        else {
-            $('.scroll').removeClass("fixed");
-        }
-
-        if (
-            $(window).scrollTop() >= $(".sale").offset().top
-            &&
-            $(window).scrollTop() + $(window).height() <= $(".method").offset().top
-        )
-        {
-            $(".float").fadeIn();
-        } else {
-            $(".float").fadeOut();
-        }
-    });
-});
-
 //選單下滑動態
 $(function(){
     $('#to-agenda').click(function(){
@@ -42,4 +7,3 @@ $(function(){
         $('body,html').animate({scrollTop:$('#fashion-content').offset().top-150}, 800);
     });
 });
-
