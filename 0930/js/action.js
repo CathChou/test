1d85d81702e83aaca8368424fa9dce9ea7e93857
cath <claire32186@gmail.com>
2021-09-30 11:16:19 +0800

    0930

--- 0930/js/action.js
+++ 0930/js/action.js
@@ -1,55 +1,45 @@
+
 //reload
-var windowWidth = $(window).width();
-    $(window).resize(function() {
-    if(windowWidth != $(window).width()){
-    location.reload();
-    return;
-    }
-});
-
-
-//手機板選單收合
-var ww = $(window).width();
-$(document).ready(function() {
-    $(".toggle-nav").click(function(e) {
-        e.preventDefault();
-        $(this).toggleClass("");
-        $(".navigation ul").toggle();
-    });
-    adjustMenu();
-})
-$(window).bind('resize orientationchange', function() {
-    ww = document.body.clientWidth;
-    adjustMenu();
-});
-var adjustMenu = function() {
-    if (ww < 1024) {
-    $(".navigation ul li").unbind('mouseenter mouseleave');
-    $(".navigation ul li a, .logo a").click(function(){
-            $(".navigation ul").hide();
-    }); 
-    }
-}    
-
+//var windowWidth = $(window).width();
+    //$(window).resize(function() {
+    //if(windowWidth != $(window).width()){
+    //location.reload();
+    //return;
+    //}
+//});
 //選單下滑動態
 $(function(){
-    $('#bt-kv, #bts-kv').click(function(){
-        $('body,html').animate({scrollTop:$('#kvc').offset().top}, 800);
+    $('#s2').click(function(){
+        $('body,html').animate({scrollTop:$('#introduction').offset().top-85}, 800);
     });
-    $('#bt-qa, #bt-scroll').click(function(){
-        $('body,html').animate({scrollTop:$('#qac').offset().top-50}, 800);
-    });
-    $('#bt-cough').click(function(){
-        $('body,html').animate({scrollTop:$('#coughc').offset().top+450}, 800);
-    });
-    $('#bt-cancer').click(function(){
-        $('body,html').animate({scrollTop:$('#cancerc').offset().top+50}, 800);
-    });
-    $('#bt-food').click(function(){
-        $('body,html').animate({scrollTop:$('#foodc').offset().top}, 800);
-    });
-    $('#bt-dad').click(function(){
-        $('body,html').animate({scrollTop:$('#dadc').offset().top}, 800);
+    $('#s3').click(function(){
+        $('body,html').animate({scrollTop:$('#introduction').offset().top-85}, 800);
     });
 });
+
+//backtotop
+$(document).ready(function(){
+    $("#back-top").hide();
+    $(function () {
+    $(window).scroll(function () {
+    if ($(this).scrollTop() > 400) {
+    $('#back-top').fadeIn();
+    } else {
+    $('#back-top').fadeOut();
+    }
+});
+    $('#back-top a').click(function () {
+        $('body,html').animate({
+        scrollTop: 0
+        }, 800);
+        return false;
+        });
+    });
+});
+
+
+
+
+
+
 
